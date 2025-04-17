9c6ea3f88580d1885fb8c6dcf86a4c39cb4604e1
koushikpr <karanboltz@gmail.com>
2025-04-17 10:36:07 -0400

    Added Docker-compose file

--- app.py
+++ app.py
@@ -4,12 +4,12 @@ from models import mongo, get_user_by_email
 from news import news_bp
 from kanban import kanban_bp
 from auth import auth_bp
-
+import os
 app = Flask(__name__)
 app.secret_key = 'supersecretdevkey123'
 
 # ✅ Set this BEFORE initializing mongo
-app.config['MONGO_URI'] = 'mongodb://localhost:27017/devsecops'
+app.config['MONGO_URI'] = app.config['MONGO_URI'] = os.getenv('MONGO_URI', 'mongodb://localhost:27017/devsecops')
 app.config['NEWS_API_KEY'] = '42a144afe912471d84a88d42f718c10b'
 
 # ✅ Init mongo after config is set
